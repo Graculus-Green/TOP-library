6a5477a92aa8dd1070a2897c6ea09f928a111d37
Graculus-Green <alex.b.blackburn@gmail.com>
2021-10-08 11:56:27 +0100

    Added some display functions

--- script.js
+++ script.js
@@ -3,36 +3,66 @@ let myLibrary = [{
     author:"Some Writer",
     pages: 297,
     read: true,
-}];
+},
+{
+    title:"Another Book",
+    author:"Another Person",
+    pages: 603,
+    read: false,
+},];
 
+
+let form = document.querySelector("form");
 let addTitle = document.querySelector('#title');
 let addAuthor = document.querySelector('#author');
 let addPages = document.querySelector('#pages');
 let addRead = document.querySelector('#read');
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    haveRead = "";
-        if(read === true)
-            haveRead = 'have read';
-        else
-            haveRead = 'have not read yet';
-                
-    
-    this.info = () => {
-        return `${title} by ${author}, ${pages} pages, ${haveRead}.`
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
-}
+};
 
 let addBookToLibrary = () => {
-    document.querySelector('#submit').addEventListener('click', e => {
-        addBook = new Book(addTitle.value, addAuthor.value, addPages.value, addRead.value);
-        myLibrary.push(addBook);
+    document.querySelector('#submit').addEventListener('click', (e) => {
+
+        title = addTitle.value;
+        author = addAuthor.value;
+        pages = addPages.value;
+        read = addRead.value
+
+        newBook = new Book(title, author, pages, read)
+        myList.push(newBook)
         
+        
+        return displayLib;
     });
-    document.querySelector('.lib').innerHTML = myLibrary;
-    console.log(myLibrary);
 }
+
+
+let displayLib = (Book) => {
+    let tableRef = document.querySelector('.lib-table');
+    myLibrary.forEach (book => {
+        let newRow = tableRef.insertRow(-1);
+
+        let newtitle = newRow.insertCell();
+        newtitle.innerHTML = book.title;
+
+        let newAuthor = newRow.insertCell();
+        newAuthor.innerHTML = book.author;
+
+        let newPages = newRow.insertCell();
+        newPages.innerHTML = book.pages;
+
+        let newRead = newRow.insertCell();
+        newRead.innerHTML = book.read;
+    }
+    )
+    
+}
+    
+displayLib();
