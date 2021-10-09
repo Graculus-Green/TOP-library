b94041509624e17382556a7eff83e15fcab2a81b
Graculus-Green <alex.b.blackburn@gmail.com>
2021-10-09 14:09:03 +0100

    Added functional delete buttons

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ let addAuthor = document.querySelector('#author');
 let addPages = document.querySelector('#pages');
 let addRead = document.querySelector('#read');
 
+
 class Book {
     constructor (title, author, pages, read) {
         this.title = title;
@@ -24,6 +25,13 @@ class Book {
         this.pages = pages;
         this.read = read;
     }
+    delete(book)  {
+        // Delete the current row
+        this.myLibrary.splice(book, 1);
+        // Display the new list
+        displayLib();
+       };
+    
 };
 
 let isBookInLib = (book) => {
@@ -34,30 +42,55 @@ let isBookInLib = (book) => {
 
 let addBookToLibrary = () => {
     document.querySelector('.submit-btn').addEventListener('click', (e) => {
-        e.preventDefault();
-        title = addTitle.value;
-        author = addAuthor.value;
-        pages = addPages.value;
+        if (addTitle.value !== ""){
+            if (addAuthor.value !== "") {
+                e.preventDefault();
+                title = addTitle.value;
+                author = addAuthor.value;
+                pages = addPages.value;
 
-        if (document.querySelector("#read-check").checked === true) {
-            read = "Yes";
+                if (document.querySelector("#read-check").checked === true) {
+                    read = "Yes";
+                }
+                else {
+                    read = "No";
+                }
+                newBook = new Book(title, author, pages, read);
+                if (isBookInLib(newBook) === false){
+                myLibrary.push(newBook);
+                }
+                
+                displayLib();
+                addTitle.value = "";
+                addAuthor.value = "";
+                addPages.value = 0;
+            }
+            else {
+                alert ("Please insert an author");
+            }
         }
         else {
-            read = "No";
+            alert ("Please insert a title!");
         }
-        newBook = new Book(title, author, pages, read);
-        if (isBookInLib(newBook) === false){
-        myLibrary.push(newBook);
-        }
-          
-        displayLib();
     });
-}
+};
+
+
+let editTable = () => {
+
+};
+let deleteBook = (book) => {
+    let reallyDel = confirm("Are you sure you want to delete?");
+    if (reallyDel) {
+        myLibrary.splice(book,1);
+        displayLib(); 
+    }
+};
 
 let displayLib = (Book) => {
     let tableRef = document.querySelector('#table-body');
     tableRef.innerHTML = '';
-
+    let bookNum = 0;
     myLibrary.forEach (book => {
         let newRow = tableRef.insertRow(-1);
 
@@ -72,9 +105,20 @@ let displayLib = (Book) => {
 
         let newRead = newRow.insertCell();
         newRead.innerHTML = book.read;
-    }
-    )
-    
-};
+
+        let newDel = newRow.insertCell();
+
+        const button = document.createElement("button");
+        button.class = "delete-btn";
+        button.id = bookNum;
+        button.innerHTML ="&#10006;";
+        newDel.appendChild(button)
+        button.onclick = function(){deleteBook(Number(button.id))};
+        console.log(myLibrary[bookNum]);
+        bookNum ++;
+});
+}
+
+
 displayLib();
 addBookToLibrary();
